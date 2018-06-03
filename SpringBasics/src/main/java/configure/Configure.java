803f7c3a8f758d6d509dcc8a0c750429131d4bcf
= <=sunillangtad54@gmail.com>
2018-06-03 21:09:09 +0530

    Autowires Music object into SoundSystem based on class name (CDPlayer)

--- SpringBasics/src/main/java/configure/Configure.java
+++ SpringBasics/src/main/java/configure/Configure.java
@@ -1,27 +1,10 @@
 package configure;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import sound.CDPlayer;
-import sound.SoundSystem;
-import sound.Walkman;
 
 
 @Configuration
+@ComponentScan(basePackages = "sound")
 public class Configure {
-
-    @Bean
-    public CDPlayer cdPlayer(){
-        return new CDPlayer();
-    }
-
-    @Bean
-    public Walkman walkman() {
-        return new Walkman();
-    }
-
-    @Bean
-    public SoundSystem soundSystem(){
-        return new SoundSystem(new Walkman());
-    }
 }
--- SpringBasics/src/main/java/sound/SoundSystem.java
+++ SpringBasics/src/main/java/sound/SoundSystem.java
@@ -1,5 +1,6 @@
 package sound;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,6 +8,7 @@ public class SoundSystem {
 
     private Music CDPlayer;
 
+    @Autowired
     public SoundSystem(Music CDPlayer) {
         this.CDPlayer = CDPlayer;
     }
